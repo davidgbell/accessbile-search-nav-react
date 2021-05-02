92755aa2aee43158b6f52d7a53e3c805e1405aab
davidgeorgebell <bell.dave93@gmail.com>
2021-05-02 17:00:02 +0800

    basic search

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,52 @@
-import Head from 'next/head';
-import Link from 'next/head';
+import { useState } from 'react';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [term, setTerm] = useState('');
+  const [searchedResult, setSearchedResult] = useState('');
+
+  const items = [
+    { name: 'apple' },
+    { name: 'banana' },
+    { name: 'pear' },
+    { name: 'apple tango' },
+    { name: 'mango' },
+    { name: 'banana milkshake' },
+  ];
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setSearchedResult(term);
+    setTerm('');
+  };
+
+  const handleChange = e => {
+    setTerm(e.target.value);
+  };
+
+  const filteredItems = items.filter(item =>
+    item.name.toLocaleLowerCase().includes(searchedResult.toLocaleLowerCase())
+  );
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <div>
           <a href='/'>LOGO</a>
         </div>
-        <input type='search' />
+        <form className={styles.search} onSubmit={handleSubmit}>
+          <input
+            type='text'
+            id='search'
+            name='search'
+            onChange={handleChange}
+            value={term || ''}
+            placeholder='search'
+          />
+          <button type='submit' onClick={handleSubmit}>
+            Search
+          </button>
+        </form>
         <nav className={styles.nav}>
           <div>
             <a href='/'>About</a>
@@ -20,9 +57,14 @@ export default function Home() {
         </nav>
       </header>
       <main className={styles.main}>
-        <ul>
-          <li>Item</li>
-        </ul>
+        <h3>Items</h3>
+        {filteredItems && (
+          <ul>
+            {filteredItems.map(item => (
+              <li key={item.name}>{item.name}</li>
+            ))}
+          </ul>
+        )}
       </main>
     </div>
   );
